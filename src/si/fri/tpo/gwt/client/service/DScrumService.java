5c1e68efa28aeeea771e650514ece7c7ba0d700e
JakaCikac <nanorax@gmail.com>
2014-04-04 20:43:02 +0200

    Adding service
    Adding service folder that includes the DScrum interface and a DScrum interface for asynchronous calls.

---
+++ src/si/fri/tpo/gwt/client/service/DScrumService.java
@@ -0,0 +1,13 @@
+package si.fri.tpo.gwt.client.service;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * Created by nanorax on 04/04/14.
+ */
+
+@RemoteServiceRelativePath("dscrum")
+public interface DScrumService extends RemoteService {
+
+}
---
+++ src/si/fri/tpo/gwt/client/service/DScrumServiceAsync.java
@@ -0,0 +1,12 @@
+package si.fri.tpo.gwt.client.service;
+
+import com.extjs.gxt.ui.client.widget.form.Time;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * Created by nanorax on 04/04/14.
+ */
+public interface DScrumServiceAsync {
+
+}
+
