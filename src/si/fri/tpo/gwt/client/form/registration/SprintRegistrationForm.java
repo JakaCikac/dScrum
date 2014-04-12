11e8bad1c2aa6097463f09a55d83ecc5f211c543
Anye Schwarzmann <anze.schwarzmann@gmail.com>
2014-04-12 11:40:53 +0200

    sprint_temp

--- src/si/fri/tpo/gwt/client/form/registration/SprintRegistrationForm.java
+++ src/si/fri/tpo/gwt/client/form/registration/SprintRegistrationForm.java
@@ -1,12 +1,14 @@
 package si.fri.tpo.gwt.client.form.registration;
 
 import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
 import si.fri.tpo.gwt.client.service.DScrumServiceAsync;
 
 /**
  * Created by Administrator on 07-Apr-14.
  */
-public class SprintRegistrationForm extends AbstractSprintRegistrationForm {
+public class SprintRegistrationForm implements IsWidget {
 
     public SprintRegistrationForm(DScrumServiceAsync service) {
         super(service);
@@ -19,5 +21,10 @@ public class SprintRegistrationForm extends AbstractSprintRegistrationForm {
         super.onRender(parent, index);
         initNewRegistrationForm();
         initComponentsDataFill();
+    }
+
+    @Override
+    public Widget asWidget() {
+        return null;
     }
 }
