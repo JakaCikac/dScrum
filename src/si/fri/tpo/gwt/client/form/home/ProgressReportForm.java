ba0db0f3dc70a22184f98272068160efe84c0084
t13db <denix8@gmail.com>
2014-05-21 23:24:43 +0200

    In the middle of something

--- src/si/fri/tpo/gwt/client/form/home/ProgressReportForm.java
+++ src/si/fri/tpo/gwt/client/form/home/ProgressReportForm.java
@@ -6,19 +6,46 @@ import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.widget.core.client.ContentPanel;
+import si.fri.tpo.gwt.client.dto.SprintDTO;
 import si.fri.tpo.gwt.client.dto.TaskDTO;
 import si.fri.tpo.gwt.client.dto.UserStoryDTO;
-import si.fri.tpo.gwt.client.dto.WorkblockDTO;
 import si.fri.tpo.gwt.client.dto.WorkloadDTO;
 import si.fri.tpo.gwt.client.service.DScrumServiceAsync;
 import si.fri.tpo.gwt.client.session.SessionInfo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Created by t13db on 19.5.2014.
  */
 public class ProgressReportForm implements IsWidget {
+
+    class BurndownData {
+
+        private Date date;
+        private int hrsRemaining;
+        private int hrsSpent;
+
+        public BurndownData(Date date, int hrsRemaining, int hrsSpent) {
+            this.date = date;
+            this.hrsRemaining = hrsRemaining;
+            this.hrsSpent = hrsSpent;
+        }
+    }
+
+    class BurndownData2 {
+
+        private int hrsRemaining;
+        private int hrsSpent;
+
+        public BurndownData2(int hrsRemaining, int hrsSpent) {
+            this.hrsRemaining = hrsRemaining;
+            this.hrsSpent = hrsSpent;
+        }
+    }
 
     private ContentPanel center, west, east, north, south;
     private DScrumServiceAsync service;
@@ -45,30 +72,50 @@ public class ProgressReportForm implements IsWidget {
 
     private void createBurnDownForm() {
 
+
+
+        final ListStore<BurndownData> store = new ListStore<BurndownData>(getModelKeyProvider());
+        //store.addAll();
+    }
+
+    private ModelKeyProvider<BurndownData> getModelKeyProvider() {
+        ModelKeyProvider<BurndownData> mkp = new ModelKeyProvider<BurndownData>() {
+            @Override
+            public String getKey(BurndownData item) {
+                return item.date.toString();
+            }
+        };
+        return mkp;
+    }
+
+    private List<BurndownData> fillBurndownData() {
+
+        List<BurndownData> burndownDataList = new ArrayList<BurndownData>();
+        Date startDate = null;
+        Date endDate = new Date();
+
+        TreeMap<Date, BurndownData2> temp = new TreeMap<Date, BurndownData2>();
+
+        // get/find start date
+        for (SprintDTO sprintDTO : SessionInfo.projectDTO.getSprintList()) {
+            if (startDate.after(sprintDTO.getStartDate()) || startDate == null) {
+                startDate = sprintDTO.getStartDate();
+            }
+        }
+
+        // fetch/accumulate data
         for (UserStoryDTO userStoryDTO : SessionInfo.projectDTO.getUserStoryList()) {
             for (TaskDTO taskDTO : userStoryDTO.getTaskList()) {
                 for (WorkloadDTO workloadDTO : taskDTO.getWorkloadList()) {
-                    if (workloadDTO.getUser() == SessionInfo.userDTO) {
-                        for (WorkblockDTO workblockDTO : workloadDTO.getWorkblockList()) {
-                            Date startDateTime = workblockDTO.getTimeStart();
-                            Date endDateTime = workblockDTO.getTimeStop();
-                        }
-                    }
+                    int timeRemaining = Integer.parseInt(workloadDTO.getTimeRemaining());
+                    int timeSpent = Integer.parseInt(workloadDTO.getTimeSpent());
+                    Date date = new Date();// = workloadDTO.getDate();
+                    BurndownData2 timeParams = new BurndownData2(timeRemaining, timeSpent);
+                    temp.put(date, timeParams);
                 }
             }
         }
 
-        final ListStore<Integer> store = new ListStore<Integer>(getModelKeyProvider());
-        store.addAll(TestData.getData(8, 20, 100));
-    }
-
-    private ModelKeyProvider<Integer> getModelKeyProvider() {
-        ModelKeyProvider<Integer> mkp = new ModelKeyProvider<Integer>() {
-            @Override
-            public String getKey(WorkblockDTO item) {
-                return item.getWorkblockPK().getWorkblockId() + "";
-            }
-        };
-        return mkp;
+        return burndownDataList;
     }
 }
